334a91225aed947dcdd338a2652e527532c8e1b9
JonathanPeña <118032290+JonathanPR3@users.noreply.github.com>
2025-01-20 18:23:43 -0600

    Push inicial

---
+++ graduados/src/main/java/com/example/graduados/GraduadosApplication.java
@@ -0,0 +1,13 @@
+package com.example.graduados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduadosApplication.class, args);
+	}
+
+}
---
+++ graduados/src/test/java/com/example/graduados/GraduadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graduados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
