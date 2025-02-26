1bc3217df274e9b9fb27159a99ef54103721559d
JonathanPeña <jonitopera777@gmail.com>
2025-02-25 21:40:26 -0600

    Pdf correccio
    Correccion pdf

--- graduados/src/main/java/com/example/graduados/services/PdfService.java
+++ graduados/src/main/java/com/example/graduados/services/PdfService.java
@@ -11,6 +11,10 @@ import java.io.IOException;
 @Service
 public class PdfService {
 
+    /**
+     * @param graduado
+     * @param numAcompanantes
+     */
     public void generarInvitaciones(Graduado graduado, int numAcompanantes) {
         Document document = new Document();
 
@@ -29,13 +33,23 @@ public class PdfService {
             document.open();
 
             // Fuentes personalizadas
-            Font tituloFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 18, Font.BOLD, BaseColor.WHITE);
-            Font contenidoFont = FontFactory.getFont(FontFactory.HELVETICA, 12, BaseColor.WHITE);
+            Font tituloFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 18, Font.BOLD, BaseColor.BLACK); // Cambiado a negro
+            Font contenidoFont = FontFactory.getFont(FontFactory.HELVETICA, 12, BaseColor.BLACK); // Cambiado a negro
+
+            // Agregar espacio en blanco para mover el contenido más abajo
+            for (int i = 0; i < 17; i++) {
+                document.add(new Paragraph(" ")); // Agrega espacios en blanco
+            }
 
             // Título de la invitación
             Paragraph titulo = new Paragraph("Invitación para el Graduado", tituloFont);
             titulo.setAlignment(Paragraph.ALIGN_CENTER);
             document.add(titulo);
+
+            Paragraph space= new Paragraph(" ");
+            document.add(space);
+
+
 
             // Tabla con la información del graduado
             PdfPTable table = new PdfPTable(2);
@@ -60,9 +74,16 @@ public class PdfService {
 
             // Invitaciones de los acompañantes
             for (int i = 1; i <= numAcompanantes; i++) {
+                // Agregar espacio en blanco para mover el contenido más abajo
+                for (int j = 0; j < 17; j++) {
+                    document.add(new Paragraph(" ")); // Agrega espacios en blanco
+                }
+
                 Paragraph tituloAcompanante = new Paragraph("Invitación para Acompañante " + i, tituloFont);
                 tituloAcompanante.setAlignment(Paragraph.ALIGN_CENTER);
                 document.add(tituloAcompanante);
+                document.add(space);
+
 
                 Paragraph infoAcompanante = new Paragraph("Este es un acompañante del graduado: " + graduado.getNombre(), contenidoFont);
                 document.add(infoAcompanante);
